e19cb16025953cee86cbe587d5ad788d45193074
Tomas Trnecka <trnecka@gmail.com>
2026-06-16 17:06:42 +0200

    Updated avatar and focus

--- terraform/main.tf
+++ terraform/main.tf
@@ -264,7 +264,7 @@ resource "aws_cloudfront_distribution" "main" {
 
   custom_error_response {
     error_code         = 404
-    response_code      = 200
-    response_page_path = "/index.html"
+    response_code      = 404
+    response_page_path = "/404.html"
   }
 }
